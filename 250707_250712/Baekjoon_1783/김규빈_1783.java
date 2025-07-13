03b277250159c422e63d290b38bfc3e6f3a982c5
pbh2709 <154939304+pbh2709@users.noreply.github.com>
2025-07-13 16:37:48 +0900

    Update 김규빈_1783.java

--- 250707_250712/Baekjoon_1783/김규빈_1783.java
+++ 250707_250712/Baekjoon_1783/김규빈_1783.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class 김규빈_1783{
+public class 김규빈_1783{  //116ms
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
